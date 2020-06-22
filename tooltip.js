e4125f05e48c08d0bae8e9d82da6fc1e34e03d74
Akshay Bagai <Akshay.Bagai@ey.com>
2020-06-21 22:09:21 -0400

    added a tooltip

--- tooltip.js
+++ tooltip.js
@@ -1,12 +1,26 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
+    this._toolTipContainer;
   }
 
   connectedCallback() {
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?)';
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
     this.appendChild(tooltipIcon);
+  }
+
+  //_ indicates that you should not call this method from outside
+  _showTooltip(){
+    this._toolTipContainer = document.createElement('div');
+    this._toolTipContainer.textContent = 'This is a tool tip';
+    this.appendChild(this._toolTipContainer);
+  }
+
+  _hideTooltip(){
+    this.removeChild(this._toolTipContainer);
   }
 }
 
